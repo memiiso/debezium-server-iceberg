021568a59504167a1a38a3e93e51db55dee9c7c2
ismail simsek <gel.yine.gel@hotmail.com>
2021-04-03 13:52:58 +0200

    partition table by destination and hour (#5)

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergEventsChangeConsumerTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergEventsChangeConsumerTest.java
@@ -20,6 +20,8 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.UUID;
@@ -38,6 +40,7 @@ import org.apache.iceberg.io.FileAppender;
 import org.apache.iceberg.io.OutputFile;
 import org.apache.iceberg.parquet.Parquet;
 import org.apache.iceberg.types.Types;
+import org.apache.iceberg.util.DateTimeUtil;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.awaitility.Awaitility;
@@ -90,75 +93,86 @@ public class IcebergEventsChangeConsumerTest extends BaseSparkTest {
   }
 
   @Test
-  // @TODO bring back date time filed, use it for partitioning data
-  public void testAppend() throws InterruptedException {
+  public void testExampleAppend() throws InterruptedException {
 
-      Schema TABLE_SCHEMA = new Schema(
-          required(1, "event_destination", Types.StringType.get()),
-          optional(2, "event_key", Types.StringType.get()),
-          optional(3, "event_value", Types.StringType.get()),
-          optional(4, "event_sink_epoch_ms", Types.LongType.get()));
-      PartitionSpec TABLE_PARTITION = PartitionSpec.unpartitioned();
-      TableIdentifier tableIdentifier = TableIdentifier.of(Namespace.of("default"), "debezium_events_test");
+    Schema TABLE_SCHEMA = new Schema(
+        required(1, "event_destination", Types.StringType.get()),
+        optional(2, "event_key", Types.StringType.get()),
+        optional(3, "event_value", Types.StringType.get()),
+        optional(4, "event_sink_epoch_ms", Types.LongType.get()),
+        optional(5, "event_sink_timestamp", Types.TimestampType.withZone())
+    );
+    PartitionSpec TABLE_PARTITION = PartitionSpec.builderFor(TABLE_SCHEMA)
+        .identity("event_destination")
+        .hour("event_sink_timestamp")
+        .build();
 
-      Map<String, String> conf = IcebergUtil.getConfigSubset(ConfigProvider.getConfig(), PROP_PREFIX);
-      conf.forEach(this.hadoopConf::set);
-      conf.forEach(this.icebergProperties::put);
+    TableIdentifier tableIdentifier = TableIdentifier.of(Namespace.of("default"), "debezium_events_test");
 
-      Catalog icebergCatalog = CatalogUtil.buildIcebergCatalog(catalogName, icebergProperties, hadoopConf);
-      icebergCatalog.createTable(tableIdentifier, TABLE_SCHEMA, TABLE_PARTITION);
-      Table eventTable = icebergCatalog.loadTable(tableIdentifier);
+    Map<String, String> conf = IcebergUtil.getConfigSubset(ConfigProvider.getConfig(), PROP_PREFIX);
+    conf.forEach(this.hadoopConf::set);
+    conf.forEach(this.icebergProperties::put);
 
-      final String fileName =
-          UUID.randomUUID() + "-" + Instant.now().toEpochMilli() + "." + FileFormat.PARQUET;
-      // NOTE! manually setting partition directory here to destination
-      OutputFile out = eventTable.io().newOutputFile(eventTable.locationProvider().newDataLocation(fileName));
+    Catalog icebergCatalog = CatalogUtil.buildIcebergCatalog(catalogName, icebergProperties, hadoopConf);
+    icebergCatalog.createTable(tableIdentifier, TABLE_SCHEMA, TABLE_PARTITION);
+    Table eventTable = icebergCatalog.loadTable(tableIdentifier);
 
-      FileAppender<Record> writer;
-      try {
-        writer = Parquet.write(out)
-            .createWriterFunc(GenericParquetWriter::buildWriter)
-            .forTable(eventTable)
-            .overwrite()
-            .build();
+    final String fileName =
+        UUID.randomUUID() + "-" + Instant.now().toEpochMilli() + "." + FileFormat.PARQUET;
+    // NOTE! manually setting partition directory here to destination
+    OutputFile out = eventTable.io().newOutputFile(eventTable.locationProvider().newDataLocation(fileName));
 
-        try (Closeable toClose = writer) {
-          GenericRecord rec = GenericRecord.create(TABLE_SCHEMA.asStruct());
-          rec.setField("event_destination", "test_destination");
-          rec.setField("event_key", "{\"aaa\":\"event_key_data\"}");
-          rec.setField("event_value", "event_value_data");
-          rec.setField("event_sink_epoch_ms", Instant.now().toEpochMilli());
-          ArrayList<Record> a = new ArrayList<>();
-          a.add(rec);
-          writer.addAll(a);
-        }
-
-      } catch (IOException e) {
-        LOGGER.error("Failed committing events to iceberg table!", e);
-        throw new InterruptedException(e.getMessage());
-      }
-
-      PartitionKey pk = new PartitionKey(TABLE_PARTITION, TABLE_SCHEMA);
-      Record pr = GenericRecord.create(TABLE_SCHEMA).copy("event_destination", "test_destination");
-      pk.partition(pr);
-
-      DataFile dataFile = DataFiles.builder(eventTable.spec())
-          .withFormat(FileFormat.PARQUET)
-          .withPath(out.location())
-          .withFileSizeInBytes(writer.length())
-          .withSplitOffsets(writer.splitOffsets())
-          .withMetrics(writer.metrics())
-          //.withPartition(pk)
+    FileAppender<Record> writer;
+    try {
+      writer = Parquet.write(out)
+          .createWriterFunc(GenericParquetWriter::buildWriter)
+          .forTable(eventTable)
+          .overwrite()
           .build();
 
-      LOGGER.debug("Appending new file '{}' !", dataFile.path());
-      eventTable.newAppend()
-          .appendFile(dataFile)
-          .commit();
-      LOGGER.info("Committed events to table! {}", eventTable.location());
+      try (Closeable toClose = writer) {
+        GenericRecord rec = GenericRecord.create(TABLE_SCHEMA.asStruct());
+        rec.setField("event_destination", "test_destination");
+        rec.setField("event_key", "{\"aaa\":\"event_key_data\"}");
+        rec.setField("event_value", "event_value_data");
+        rec.setField("event_sink_epoch_ms", Instant.now().toEpochMilli());
+        rec.setField("event_sink_timestamp", OffsetDateTime.now(ZoneOffset.UTC));
+        ArrayList<Record> a = new ArrayList<>();
+        a.add(rec);
+        writer.addAll(a);
+      }
 
+    } catch (IOException e) {
+      LOGGER.error("Failed committing events to iceberg table!", e);
+      throw new InterruptedException(e.getMessage());
+    }
+
+    PartitionKey pk = new PartitionKey(TABLE_PARTITION, TABLE_SCHEMA);
+    Record pr = GenericRecord.create(TABLE_SCHEMA);
+    pr.setField("event_destination", "test_destination");
+    pr.setField("event_sink_timestamp", DateTimeUtil.microsFromTimestamptz(OffsetDateTime.now(ZoneOffset.UTC)));
+    pk.partition(pr);
+    System.out.println(pk.toPath());
+    System.out.println(pk.toPath());
+
+    DataFile dataFile = DataFiles.builder(eventTable.spec())
+        .withFormat(FileFormat.PARQUET)
+        .withPath(out.location())
+        .withFileSizeInBytes(writer.length())
+        .withSplitOffsets(writer.splitOffsets())
+        .withMetrics(writer.metrics())
+        .withPartition(pk)
+        .build();
+
+    LOGGER.debug("Appending new file '{}' !", dataFile.path());
+    eventTable.newAppend()
+        .appendFile(dataFile)
+        .commit();
+    LOGGER.info("Committed events to table! {}", eventTable.location());
+
+    S3Minio.listFiles();
     Dataset<Row> ds = spark.newSession().sql("SELECT * FROM default.debezium_events_test");
-      ds.show();
+    ds.show();
   }
 
 }
