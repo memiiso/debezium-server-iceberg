ea52005ea9012c94b49d1355feda582197d10fa2
Ismail Simsek <ismail.simsek@ppro.com>
2021-04-07 23:54:48 +0200

    prepare Upsert Logic

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergChangeConsumerUpsertTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergChangeConsumerUpsertTest.java
@@ -14,7 +14,6 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +21,7 @@ import javax.inject.Inject;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.awaitility.Awaitility;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -41,7 +39,6 @@ public class IcebergChangeConsumerUpsertTest extends BaseSparkTest {
   IcebergChangeConsumer consumer;
 
   @Test
-  @Disabled
   public void testSimpleUpsert() throws Exception {
 
     List<ChangeEvent<Object, Object>> records = new ArrayList<>();
@@ -50,18 +47,75 @@ public class IcebergChangeConsumerUpsertTest extends BaseSparkTest {
     records.add(getCustomerRecord(3, "c"));
     consumer.handleBatch(records, TestUtil.getCommitter());
 
-    Awaitility.await().atMost(Duration.ofSeconds(ConfigSource.waitForSeconds())).until(() -> {
-      try {
-        Dataset<Row> ds = getTableData("testc.inventory.customers");
-        ds.show();
-        return ds.count() >= 3;
-      } catch (Exception e) {
-        return false;
-      }
-    });
+    Dataset<Row> ds = getTableData("testc.inventory.customers_upsert");
+    Assertions.assertEquals(ds.count(), 3);
+    Assertions.assertEquals(ds.where("id == 3").count(), 1);
+
+    // 3 records should be updated 4th one should be inserted
+    records.clear();
+    records.add(getCustomerRecord(1, "r"));
+    records.add(getCustomerRecord(2, "d"));
+    records.add(getCustomerRecord(3, "u", "UpdatednameV1"));
+    records.add(getCustomerRecord(4, "c"));
+    consumer.handleBatch(records, TestUtil.getCommitter());
+
+    ds = getTableData("testc.inventory.customers_upsert");
+    ds.show();
+    Assertions.assertEquals(ds.count(), 4);
+    Assertions.assertEquals(ds.where("id == 1 AND __op= 'r'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 2 AND __op= 'd'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 3 AND __op= 'u'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 3 AND first_name= 'UpdatednameV1'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 4 AND __op= 'c'").count(), 1);
+
+    records.clear();
+    // incase of duplicate records should only keep the latest
+    records.add(getCustomerRecord(3, "r", "UpdatednameV2"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(3, "u", "UpdatednameV3"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(3, "u", "UpdatednameV4"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(4, "u", "Updatedname-4-V1"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(4, "u", "Updatedname-4-V2"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(4, "r", "Updatedname-4-V3"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(5, "r"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(6, "r"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(6, "r"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(6, "u"));
+    Thread.sleep(10);
+    records.add(getCustomerRecord(6, "u", "Updatedname-6-V1"));
+    consumer.handleBatch(records, TestUtil.getCommitter());
+    ds = getTableData("testc.inventory.customers_upsert");
+    ds.show();
+    Assertions.assertEquals(ds.count(), 6);
+    Assertions.assertEquals(ds.where("id == 3 AND __op= 'u' AND first_name= 'UpdatednameV4'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 4 AND __op= 'r' AND first_name= 'Updatedname-4-V3'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 5 AND __op= 'r' ").count(), 1);
+    Assertions.assertEquals(ds.where("id == 6 AND __op= 'u' AND first_name= 'Updatedname-6-V1'").count(), 1);
+
+    // in case of duplicate records including ts, its should keep based on operation priority
+    // ("c", 1, "r", 2, "u", 3, "d", 4);
+    records.clear();
+    records.add(getCustomerRecord(3, "d", "UpdatednameV5"));
+    records.add(getCustomerRecord(3, "u", "UpdatednameV6"));
+    records.add(getCustomerRecord(6, "c", "Updatedname-6-V2"));
+    records.add(getCustomerRecord(6, "r", "Updatedname-6-V3"));
+    consumer.handleBatch(records, TestUtil.getCommitter());
+    ds = getTableData("testc.inventory.customers_upsert");
+    ds.show();
+    Assertions.assertEquals(ds.where("id == 3 AND __op= 'd' AND first_name= 'UpdatednameV5'").count(), 1);
+    Assertions.assertEquals(ds.where("id == 6 AND __op= 'r' AND first_name= 'Updatedname-6-V3'").count(), 1);
+
   }
 
-  private TestChangeEvent getCustomerRecord(Integer id, String operation) {
+  private TestChangeEvent<Object, Object> getCustomerRecord(Integer id, String operation, String name) {
     String key = "{\"schema\":{\"type\":\"struct\",\"fields\":[{\"type\":\"int32\",\"optional\":false," + "\"field\":\"id\"}]," +
         "\"optional\":false,\"name\":\"testc.inventory.customers.Key\"}," +
         "\"payload\":{\"id\":" + id + "}}";
@@ -71,10 +125,15 @@ public class IcebergChangeConsumerUpsertTest extends BaseSparkTest {
         "{\"type\":\"string\",\"optional\":true,\"field\":\"__table\"},{\"type\":\"int64\",\"optional\":true,\"field\":\"__lsn\"}," +
         "{\"type\":\"int64\",\"optional\":true,\"field\":\"__source_ts_ms\"},{\"type\":\"string\",\"optional\":true,\"field\":\"__deleted\"}]," +
         "\"optional\":false,\"name\":\"testc.inventory.customers.Value\"}," +
-        "\"payload\":{\"id\":" + id + ",\"first_name\":\"Edward\",\"last_name\":\"Walker\",\"email\":\"ed@walker.com\"," +
+        "\"payload\":{\"id\":" + id + ",\"first_name\":\"" + name + "\",\"last_name\":\"Walker\",\"email\":\"ed@walker" +
+        ".com\"," +
         "\"__op\":\"" + operation + "\",\"__table\":\"customers\",\"__lsn\":33832960,\"__source_ts_ms\":" + Instant.now().toEpochMilli() + "," +
         "\"__deleted\":\"" + operation.equals("d") + "\"}} ";
-    return new TestChangeEvent(key, val, "testc.inventory.customers");
+    return new TestChangeEvent<>(key, val, "testc.inventory.customers_upsert");
+  }
+
+  private TestChangeEvent<Object, Object> getCustomerRecord(Integer id, String operation) {
+    return this.getCustomerRecord(id, operation, TestUtil.randomString(12));
   }
 
 }
