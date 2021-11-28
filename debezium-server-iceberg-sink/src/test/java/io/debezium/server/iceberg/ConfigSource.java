165e4e92742cc516acedfc38d3f3f320939cb580
Ismail Simsek <gel.yine.gel@hotmail.com>
2021-11-28 23:32:29 +0100

    support array type

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/ConfigSource.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/ConfigSource.java
@@ -62,7 +62,7 @@ public class ConfigSource extends TestConfigSource {
     config.put("debezium.source.database.server.name", "testc");
     config.put("%postgresql.debezium.source.schema.whitelist", "inventory");
     config.put("%postgresql.debezium.source.database.whitelist", "inventory");
-    config.put("%postgresql.debezium.source.table.whitelist", "inventory.*");
+    config.put("debezium.source.table.whitelist", "inventory.*");
     config.put("debezium.source.include.schema.changes", "false");
 
     config.put("quarkus.log.level", "INFO");
