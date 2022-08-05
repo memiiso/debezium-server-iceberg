59d84883008535d975a5add7b8da7cc4e9a0d5b8
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-08-05 21:01:22 +0200

    Add mangodb test (#65)

---
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergChangeConsumerMangodbTest.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *  * Copyright memiiso Authors.
+ *  *
+ *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+package io.debezium.server.iceberg;
+
+import io.debezium.server.iceberg.testresources.BaseSparkTest;
+import io.debezium.server.iceberg.testresources.S3Minio;
+import io.debezium.server.iceberg.testresources.SourceMangoDB;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+
+import java.time.Duration;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration test that verifies basic reading from PostgreSQL database and writing to s3 destination.
+ *
+ * @author Ismail Simsek
+ */
+@QuarkusTest
+@QuarkusTestResource(S3Minio.class)
+@QuarkusTestResource(SourceMangoDB.class)
+@TestProfile(IcebergChangeConsumerMangodbTestProfile.class)
+public class IcebergChangeConsumerMangodbTest extends BaseSparkTest {
+
+  @Test
+  public void testSimpleUpload() {
+    
+    Awaitility.await().atMost(Duration.ofSeconds(180)).until(() -> {
+      try {
+        Dataset<Row> df = getTableData("testc.inventory.products");
+        df.show();
+        return df.filter("_id is not null").count() >= 4;
+      } catch (Exception e) {
+        return false;
+      }
+    });
+  }
+
+}
---
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/testresources/SourceMangoDB.java
@@ -0,0 +1,54 @@
+/*
+ *
+ *  * Copyright memiiso Authors.
+ *  *
+ *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+package io.debezium.server.iceberg.testresources;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+
+public class SourceMangoDB implements QuarkusTestResourceLifecycleManager {
+
+  public static final int MONGODB_PORT = 27017;
+  public static final GenericContainer<?> container = new GenericContainer(
+      new ImageFromDockerfile("debezium_mongodb", false)
+          .withFileFromClasspath("Dockerfile", "mongodb/Dockerfile")
+          .withFileFromClasspath("start-mongodb.sh", "mongodb/start-mongodb.sh"))
+
+      .waitingFor(Wait.forLogMessage(".*Successfully initialized inventory database.*", 1))
+      .withStartupTimeout(Duration.ofSeconds(60L));
+
+  @Override
+  public Map<String, String> start() {
+    container.withExposedPorts(MONGODB_PORT).start();
+
+    Map<String, String> params = new ConcurrentHashMap<>();
+    params.put("%mongodb.debezium.source.mongodb.hosts",
+        "rs0/" + container.getHost() + ":" + container.getMappedPort(MONGODB_PORT)
+    );
+    params.put("%mongodb.debezium.source.mongodb.authsource", "admin");
+    params.put("%mongodb.debezium.source.mongodb.user", "debezium");
+    params.put("%mongodb.debezium.source.mongodb.password", "dbz");
+    //params.put("%mongodb.debezium.source.mongodb.ssl.enabled", "false");
+    return params;
+  }
+
+  @Override
+  public void stop() {
+    if (container != null) {
+      container.stop();
+    }
+  }
+
+}
