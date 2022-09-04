2b9d2493d154bc0aad47b3b57b2b435552bde0f1
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-09-04 15:25:25 +0200

    Cleanup tests

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/ConfigSource.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/ConfigSource.java
@@ -44,7 +44,7 @@ public class ConfigSource extends TestConfigSource {
     // debezium unwrap message
     config.put("debezium.transforms", "unwrap");
     config.put("debezium.transforms.unwrap.type", "io.debezium.transforms.ExtractNewRecordState");
-    config.put("debezium.transforms.unwrap.add.fields", "op,table,source.ts_ms,db");
+    config.put("debezium.transforms.unwrap.add.fields", "op,table,source.ts_ms,db,ts_ms");
     config.put("debezium.transforms.unwrap.delete.handling.mode", "rewrite");
     config.put("debezium.transforms.unwrap.drop.tombstones", "true");
 
