946abf7916c227e62d3840c87032eba1c90af99a
Ismail Simsek <ismail.simsek@verivox.com>
2024-07-24 15:50:34 +0200

    Add option to change case of destination/iceberg table names

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergChangeConsumerTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/IcebergChangeConsumerTest.java
@@ -336,10 +336,10 @@ public class IcebergChangeConsumerTest extends BaseSparkTest {
     assertEquals(TableIdentifier.of(Namespace.of(namespace), "debeziumcdc_table"), icebergConsumer.mapDestination("table2"));
     icebergConsumer.destinationUppercaseTableNames = true;
     icebergConsumer.destinationLowercaseTableNames = false;
-    assertEquals(TableIdentifier.of(Namespace.of(namespace.toUpperCase()), "DEBEZIUMCDC_TABLE_LOWERCASE"), icebergConsumer.mapDestination("table_lowercase"));
+    assertEquals(TableIdentifier.of(Namespace.of(namespace), "DEBEZIUMCDC_TABLE_LOWERCASE"), icebergConsumer.mapDestination("table_lowercase"));
     icebergConsumer.destinationUppercaseTableNames = false;
     icebergConsumer.destinationLowercaseTableNames = true;
-    assertEquals(TableIdentifier.of(Namespace.of(namespace.toLowerCase()), "debeziumcdc_table_camelcase"), icebergConsumer.mapDestination("table_CamelCase"));
+    assertEquals(TableIdentifier.of(Namespace.of(namespace), "debeziumcdc_table_camelcase"), icebergConsumer.mapDestination("table_CamelCase"));
   }
 
   public static class TestProfile implements QuarkusTestProfile {
