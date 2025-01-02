a328d530f4c4682a986d5a19de82310c46577af9
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-01-02 22:48:12 +0100

    Use jdbc catalog

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/testresources/S3Minio.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/testresources/S3Minio.java
@@ -25,9 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static io.debezium.server.iceberg.TestConfigSource.S3_BUCKET;
-import static io.debezium.server.iceberg.TestConfigSource.S3_BUCKET_NAME;
-
 public class S3Minio implements QuarkusTestResourceLifecycleManager {
 
   public static final String MINIO_ACCESS_KEY = "admin";
@@ -35,6 +32,10 @@ public class S3Minio implements QuarkusTestResourceLifecycleManager {
   protected static final Logger LOGGER = LoggerFactory.getLogger(S3Minio.class);
   static final String DEFAULT_IMAGE = "minio/minio:latest";
   public static MinioClient client;
+  public static final String S3_REGION = "us-east-1";
+  public static final String ICEBERG_CATALOG_FILEIO = "org.apache.iceberg.aws.s3.S3FileIO";
+  public static final String S3_WAREHOUSE_BUCKET_NAME = "test-bucket";
+  public static final String S3_WAREHOUSE_BUCKET_DIR = "s3://" + S3_WAREHOUSE_BUCKET_NAME + "/iceberg_warehouse";
 
   static public final MinIOContainer container = new MinIOContainer(DockerImageName.parse(DEFAULT_IMAGE))
       .withUserName(MINIO_ACCESS_KEY)
@@ -76,8 +77,8 @@ public class S3Minio implements QuarkusTestResourceLifecycleManager {
     try {
       client.ignoreCertCheck();
       client.makeBucket(MakeBucketArgs.builder()
-          .region(TestConfigSource.S3_REGION)
-          .bucket(S3_BUCKET_NAME)
+          .region(S3Minio.S3_REGION)
+          .bucket(S3_WAREHOUSE_BUCKET_NAME)
           .build());
     } catch (Exception e) {
       e.printStackTrace();
@@ -89,15 +90,15 @@ public class S3Minio implements QuarkusTestResourceLifecycleManager {
     config.put("debezium.sink.iceberg.s3.path-style-access", "true");
     config.put("debezium.sink.iceberg.s3.access-key-id", S3Minio.MINIO_ACCESS_KEY);
     config.put("debezium.sink.iceberg.s3.secret-access-key", S3Minio.MINIO_SECRET_KEY);
-    config.put("debezium.sink.iceberg.client.region", TestConfigSource.S3_REGION);
-    config.put("debezium.sink.iceberg.io-impl", TestConfigSource.ICEBERG_CATALOG_FILEIO);
-    config.put("debezium.sink.iceberg.warehouse", S3_BUCKET);
+    config.put("debezium.sink.iceberg.client.region", S3Minio.S3_REGION);
+    config.put("debezium.sink.iceberg.io-impl", S3Minio.ICEBERG_CATALOG_FILEIO);
+    config.put("debezium.sink.iceberg.warehouse", S3_WAREHOUSE_BUCKET_DIR);
     // FOR HADOOP CATALOG
-    config.put("debezium.sink.iceberg.fs.s3a.endpoint", container.getS3URL());
-    config.put("debezium.sink.iceberg.fs.s3a.access.key", S3Minio.MINIO_ACCESS_KEY);
-    config.put("debezium.sink.iceberg.fs.s3a.secret.key", S3Minio.MINIO_SECRET_KEY);
-    config.put("debezium.sink.iceberg.fs.s3a.path.style.access", "true");
-    config.put("debezium.sink.iceberg.fs.defaultFS", "s3a://" + S3_BUCKET_NAME);
+//    config.put("debezium.sink.iceberg.fs.s3a.endpoint", container.getS3URL());
+//    config.put("debezium.sink.iceberg.fs.s3a.access.key", S3Minio.MINIO_ACCESS_KEY);
+//    config.put("debezium.sink.iceberg.fs.s3a.secret.key", S3Minio.MINIO_SECRET_KEY);
+//    config.put("debezium.sink.iceberg.fs.s3a.path.style.access", "true");
+//    config.put("debezium.sink.iceberg.fs.defaultFS", "s3a://" + S3_BUCKET_NAME);
 
     return config;
   }
