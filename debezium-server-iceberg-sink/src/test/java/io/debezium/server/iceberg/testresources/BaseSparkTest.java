7e235fc28847017bfcee668e2f1866f8905ee19c
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-02-14 10:34:14 +0100

    Simplify setting up spark settings, and add ResolvingFileIO to examples

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/testresources/BaseSparkTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/testresources/BaseSparkTest.java
@@ -9,7 +9,6 @@
 package io.debezium.server.iceberg.testresources;
 
 import io.debezium.server.iceberg.IcebergUtil;
-import io.debezium.server.iceberg.TestConfigSource;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.hadoop.HadoopCatalog;
 import org.apache.spark.SparkConf;
@@ -26,7 +25,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_CATALOG_TABLE_NAMESPACE;
-import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_WAREHOUSE_S3A;
 
 /**
  * Integration test that uses spark to consumer data is consumed.
@@ -38,34 +36,28 @@ public class BaseSparkTest extends BaseTest {
   protected static final SparkConf sparkconf = new SparkConf()
       .setAppName("CDC-S3-Batch-Spark-Sink")
       .setMaster("local[2]");
-  private static final String SPARK_PROP_PREFIX = "debezium.sink.sparkbatch.";
   protected static SparkSession spark;
 
   @BeforeAll
   static void setup() {
     Awaitility.setDefaultTimeout(Duration.ofMinutes(3));
     Awaitility.setDefaultPollInterval(Duration.ofSeconds(10));
-
-    Map<String, String> appSparkConf = IcebergUtil.getConfigSubset(ConfigProvider.getConfig(), SPARK_PROP_PREFIX);
-    appSparkConf.forEach(BaseSparkTest.sparkconf::set);
     sparkconf
         .set("spark.ui.enabled", "false")
         .set("spark.eventLog.enabled", "false")
         // enable iceberg SQL Extensions and Catalog
         .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-        .set("spark.sql.warehouse.dir", ICEBERG_WAREHOUSE_S3A)
-        // hadoop catalog
-        .set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
-        .set("spark.sql.catalog.spark_catalog.type", "hadoop")
-        .set("spark.sql.catalog.spark_catalog.warehouse", ICEBERG_WAREHOUSE_S3A)
-        .set("spark.sql.catalog.spark_catalog.default-namespaces", ICEBERG_CATALOG_TABLE_NAMESPACE)
-        .set("spark.sql.catalog.spark_catalog.io-impl", TestConfigSource.ICEBERG_FILEIO)
-        .set("spark.sql.catalog.spark_catalog.s3.endpoint", S3Minio.container.getS3URL())
-        .set("spark.sql.catalog.spark_catalog.s3.path-style-access", "true")
-        .set("spark.sql.catalog.spark_catalog.s3.access-key-id", TestConfigSource.S3_MINIO_ACCESS_KEY)
-        .set("spark.sql.catalog.spark_catalog.s3.secret-access-key", TestConfigSource.S3_MINIO_SECRET_KEY)
-        .set("spark.sql.catalog.spark_catalog.client.region", TestConfigSource.S3_REGION)
+        // catalog
+        .set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkCatalog")
+        .set("spark.sql.defaultCatalog", "spark_catalog")
     ;
+
+    // take current settings and use them for sparkconf
+    Map<String, String> catalogConf = IcebergUtil.getConfigSubset(ConfigProvider.getConfig(), "debezium.sink.iceberg.");
+    catalogConf.forEach((key, value) -> {
+//      System.out.println("spark.sql.catalog.spark_catalog."+key+"="+value);
+      sparkconf.set("spark.sql.catalog.spark_catalog."+key, value);
+    });
 
     BaseSparkTest.spark = SparkSession
         .builder()
@@ -73,15 +65,14 @@ public class BaseSparkTest extends BaseTest {
         .getOrCreate();
 
     BaseSparkTest.spark.sparkContext().getConf().toDebugString();
-
   }
 
   public static String dataTypeString(Dataset<Row> dataset, String colName) {
     StructField[] fields = dataset.schema().fields();
     String dataType = null;
-    for (StructField field : fields) {
-      if (field.name().equals(colName)) {
-        dataType = field.dataType().typeName();
+    for(StructField field: fields) {
+      if(field.name().equals(colName)) {
+        dataType =  field.dataType().typeName();
         break;
       }
     }
@@ -91,15 +82,15 @@ public class BaseSparkTest extends BaseTest {
   public static void PGCreateTestDataTable() throws Exception {
     // create test table
     String sql = "" +
-        "        CREATE TABLE IF NOT EXISTS inventory.test_data (\n" +
-        "            c_id INTEGER ,\n" +
-        "            c_text TEXT,\n" +
-        "            c_varchar VARCHAR" +
-        "          );";
+                 "        CREATE TABLE IF NOT EXISTS inventory.test_data (\n" +
+                 "            c_id INTEGER ,\n" +
+                 "            c_text TEXT,\n" +
+                 "            c_varchar VARCHAR" +
+                 "          );";
     SourcePostgresqlDB.runSQL(sql);
   }
 
-  public static int PGLoadTestDataTable(int numRows, boolean addRandomDelay) {
+  public int PGLoadTestDataTable(int numRows, boolean addRandomDelay) {
     int numInsert = 0;
     do {
 
@@ -109,7 +100,7 @@ public class BaseSparkTest extends BaseTest {
             Thread.sleep(TestUtil.randomInt(20000, 100000));
           }
           String sql = "INSERT INTO inventory.test_data (c_id, c_text, c_varchar ) " +
-              "VALUES ";
+                       "VALUES ";
           StringBuilder values = new StringBuilder("\n(" + TestUtil.randomInt(15, 32) + ", '" + TestUtil.randomString(524) + "', '" + TestUtil.randomString(524) + "')");
           for (int i = 0; i < 100; i++) {
             values.append("\n,(").append(TestUtil.randomInt(15, 32)).append(", '").append(TestUtil.randomString(524)).append("', '").append(TestUtil.randomString(524)).append("')");
