a02a4d6abd7436fa9d3ffb92cf840082e6fcbba4
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-02-22 13:04:45 +0100

    Add root config class, to centralize config access

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/TestConfigProducer.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/TestConfigProducer.java
@@ -7,6 +7,11 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.Config;
 import org.mockito.Mockito;
 
+/**
+ * This class provides a mocked instance of IcebergConsumerConfig for testing purposes,
+ * allowing selective overriding of configuration values while preserving the original
+ * configuration.
+ */
 public class TestConfigProducer {
   @Inject
   Config config;
