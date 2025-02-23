7c27632747eb12b380af3d8ef58a191c375635fb
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-02-23 10:55:09 +0100

    Set default time.precision.mode to isostring

--- debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/SchemaConverter.java
+++ debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/SchemaConverter.java
@@ -190,6 +190,14 @@ public class SchemaConverter {
       case "int32": // int 4 bytes
         return switch (fieldTypeName) {
           case "io.debezium.time.Date", "org.apache.kafka.connect.data.Date" -> Types.DateType.get();
+          //"Represents the number of milliseconds"
+          case "io.debezium.time.Time" -> Types.TimeType.get();
+          //"Represents the time value in microseconds
+          case "io.debezium.time.MicroTime" -> Types.TimeType.get();
+          //"Represents the time value in nanoseconds"
+          case "io.debezium.time.NanoTime" -> Types.TimeType.get();
+          //"Represents the time value in microseconds"
+          case "org.apache.kafka.connect.data.Time" -> Types.TimeType.get();
           default -> Types.IntegerType.get();
         };
       case "int64": // long 8 bytes
@@ -204,6 +212,14 @@ public class SchemaConverter {
           case "io.debezium.time.MicroTimestamp" -> Types.TimestampType.withoutZone();
           case "io.debezium.time.NanoTimestamp" -> Types.TimestampType.withoutZone();
           case "org.apache.kafka.connect.data.Timestamp" -> Types.TimestampType.withoutZone();
+          //"Represents the number of milliseconds"
+          case "io.debezium.time.Time" -> Types.TimeType.get();
+          //"Represents the time value in microseconds
+          case "io.debezium.time.MicroTime" -> Types.TimeType.get();
+          //"Represents the time value in nanoseconds"
+          case "io.debezium.time.NanoTime" -> Types.TimeType.get();
+          //"Represents the time value in microseconds"
+          case "org.apache.kafka.connect.data.Time" -> Types.TimeType.get();
           default -> Types.LongType.get();
         };
       case "float8":
@@ -222,6 +238,7 @@ public class SchemaConverter {
           case "io.debezium.time.IsoTimestamp" -> Types.TimestampType.withoutZone();
           case "io.debezium.time.ZonedTimestamp" -> Types.TimestampType.withZone();
           // case "io.debezium.time.IsoTime" -> Types.TimeType.get();
+          // case "io.debezium.time.ZonedTime" -> Types.TimeType.get();
           default -> Types.StringType.get();
         };
       case "uuid":
