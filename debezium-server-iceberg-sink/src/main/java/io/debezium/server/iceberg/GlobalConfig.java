f05de75e6fe2d5d04136c932015f7cfa68f7e94d
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-02-23 22:48:30 +0100

    Document configuration and default values

--- debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/GlobalConfig.java
+++ debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/GlobalConfig.java
@@ -2,7 +2,10 @@ package io.debezium.server.iceberg;
 
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
 import io.smallrye.config.WithParentName;
+import org.jboss.logging.Logger;
 
 @ConfigRoot
 @ConfigMapping
@@ -13,5 +16,9 @@ public interface GlobalConfig {
 
   @WithParentName
   DebeziumConfig debezium();
+
+  @WithName("quarkus.log.level")
+  @WithDefault("INFO")
+  Logger.Level quarkusLogLevel();
 
 }
