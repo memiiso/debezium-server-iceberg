ed6794cbe7ed69ca1a2375898c02e666356e9565
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-06-06 15:47:30 +0200

    Fix IcebergSchemaHistory dont delete history records

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/history/IcebergSchemaHistoryTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/history/IcebergSchemaHistoryTest.java
@@ -22,6 +22,7 @@ import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
+import java.sql.SQLException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,11 +42,22 @@ import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_CATALOG_TABLE_
 @DisabledIfEnvironmentVariable(named = "DEBEZIUM_FORMAT_VALUE", matches = "connect")
 public class IcebergSchemaHistoryTest extends BaseSparkTest {
   @Test
-  public void testSimpleUpload() {
+  public void testSimpleUpload() throws SQLException, ClassNotFoundException {
+    String sqlCreate = "CREATE TABLE IF NOT EXISTS inventory.test_schema_history_ddl (" +
+        " c_id INTEGER ," +
+        " c_data TEXT," +
+        "  PRIMARY KEY (c_id)" +
+        " );";
+    SourceMysqlDB.runSQL(sqlCreate);
+    String sqlInsert =
+        "INSERT INTO inventory.test_schema_history_ddl (c_id, c_data ) " +
+            "VALUES  (1,'data-1'),(2,'data-2'),(3,'data-3');";
+    SourceMysqlDB.runSQL(sqlInsert);
+
     Awaitility.await().atMost(Duration.ofSeconds(120)).until(() -> {
       try {
-        Dataset<Row> ds = getTableData("testc.inventory.customers");
-        return ds.count() >= 3;
+        Dataset<Row> ds = getTableData("testc.inventory.test_schema_history_ddl");
+        return ds.count() >= 2;
       } catch (Exception e) {
         return false;
       }
@@ -55,8 +67,10 @@ public class IcebergSchemaHistoryTest extends BaseSparkTest {
     Awaitility.await().atMost(Duration.ofSeconds(120)).until(() -> {
       try {
         Dataset<Row> ds = getTableData(ICEBERG_CATALOG_TABLE_NAMESPACE, "debezium_database_history_storage_table");
-//        System.out.println(ds.count());
-        return ds.count() >= 1;
+        ds.show(10, false);
+        return ds.count() > 1
+            && ds.where("history_data ILIKE '%CREATE%TABLE%test_schema_history_ddl%'").count() == 1
+            ;
       } catch (Exception e) {
         return false;
       }
@@ -73,7 +87,7 @@ public class IcebergSchemaHistoryTest extends BaseSparkTest {
 //      config.put("%mysql.debezium.source.table.whitelist", "inventory.*");
       config.put("debezium.source.schema.history.internal", "io.debezium.server.iceberg.history.IcebergSchemaHistory");
       config.put("debezium.source.schema.history.internal.iceberg.table-name", "debezium_database_history_storage_table");
-      config.put("debezium.source.table.whitelist", "inventory.customers");
+      config.put("debezium.source.table.whitelist", "inventory.test_schema_history_ddl");
       return config;
     }
 
