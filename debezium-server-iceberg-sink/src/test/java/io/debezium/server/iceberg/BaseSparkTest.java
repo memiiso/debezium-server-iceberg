b0bcdeaa5e83cc3900319965ab1d93ac7a786b69
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-06-14 20:33:18 +0200

    Replace jdbc catalog with nessie for performance

--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/BaseSparkTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/BaseSparkTest.java
@@ -50,6 +50,11 @@ public class BaseSparkTest extends BaseTest {
         .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
         .set("spark.sql.catalog.iceberg", "org.apache.iceberg.spark.SparkCatalog") // Iceberg catalog
         .set("spark.sql.defaultCatalog", "iceberg")
+//         For spark to read iceberg tables using Hadoop file IO
+//        .set("spark.hadoop.fs.s3a.endpoint", S3Minio.container.getS3URL())
+//        .set("spark.hadoop.fs.s3a.access.key", TestConfigSource.S3_MINIO_ACCESS_KEY)
+//        .set("spark.hadoop.fs.s3a.secret.key", TestConfigSource.S3_MINIO_SECRET_KEY)
+//        .set("spark.hadoop.fs.s3a.path.style.access", "true")
         .set("spark.sql.warehouse.dir", ICEBERG_WAREHOUSE_S3A)
         // Set Spark session timezone to Berlin time, for stable temporal/timestamp-tz testing
         .set("spark.sql.session.timeZone", "Europe/Berlin")
