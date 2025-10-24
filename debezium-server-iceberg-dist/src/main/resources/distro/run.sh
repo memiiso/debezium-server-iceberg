e9531288c2d40687c0242c06dd2d54e72456ff08
Sergei Nikolaev <kinolaev@gmail.com>
2025-10-24 15:58:42 +0400

    feat: scripting, jmx and metrics configs (#645)

--- debezium-server-iceberg-dist/src/main/resources/distro/run.sh
+++ debezium-server-iceberg-dist/src/main/resources/distro/run.sh
@@ -7,11 +7,7 @@
 #  */
 #
 
-if [ -z "$JAVA_HOME" ]; then
-  JAVA_BINARY="java"
-else
-  JAVA_BINARY="$JAVA_HOME/bin/java"
-fi
+LIB_PATH="lib/*"
 
 if [ "$OSTYPE" = "msys" ] || [ "$OSTYPE" = "cygwin" ]; then
   PATH_SEP=";"
@@ -19,6 +15,21 @@ else
   PATH_SEP=":"
 fi
 
+if [ -z "$JAVA_HOME" ]; then
+  JAVA_BINARY="java"
+else
+  JAVA_BINARY="$JAVA_HOME/bin/java"
+fi
+
 RUNNER=$(ls debezium-server-*runner.jar)
 
-exec $JAVA_BINARY $DEBEZIUM_OPTS $JAVA_OPTS -cp "$RUNNER"$PATH_SEP"config"$PATH_SEP"lib/*"$PATH_SEP"/opt/conf" io.debezium.server.Main
+ENABLE_DEBEZIUM_SCRIPTING=${ENABLE_DEBEZIUM_SCRIPTING:-false}
+if [[ "${ENABLE_DEBEZIUM_SCRIPTING}" == "true" ]]; then
+  LIB_PATH=$LIB_PATH$PATH_SEP"lib_opt/*"
+fi
+
+source ./jmx/enable_jmx.sh
+source ./lib_metrics/enable_exporter.sh
+
+exec "$JAVA_BINARY" $DEBEZIUM_OPTS $JAVA_OPTS -cp \
+    $RUNNER$PATH_SEP$LIB_PATH io.debezium.server.Main
