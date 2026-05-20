8cdc0a7b335ac82d9f64567adb0bfa281daf18e8
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2026-05-20 11:03:57 +0200

    fix: update logging level and improve namespace handling in Iceberg storage classes

--- debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/history/IcebergSchemaHistory.java
+++ debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/history/IcebergSchemaHistory.java
@@ -8,6 +8,9 @@
 
 package io.debezium.server.iceberg.history;
 
+import static org.apache.iceberg.types.Types.NestedField.optional;
+import static org.apache.iceberg.types.Types.NestedField.required;
+
 import io.debezium.DebeziumException;
 import io.debezium.annotation.ThreadSafe;
 import io.debezium.common.annotation.Incubating;
@@ -23,6 +26,17 @@ import io.debezium.relational.history.SchemaHistoryListener;
 import io.debezium.server.iceberg.IcebergUtil;
 import io.debezium.util.FunctionalReadWriteLock;
 import io.debezium.util.Strings;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import org.apache.iceberg.FileFormat;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
@@ -42,21 +56,6 @@ import org.apache.iceberg.types.Types;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.Arrays;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-
-import static org.apache.iceberg.types.Types.NestedField.optional;
-import static org.apache.iceberg.types.Types.NestedField.required;
-
 /**
  * A {@link SchemaHistory} implementation that stores the schema history to Iceberg table
  *
@@ -66,12 +65,11 @@ import static org.apache.iceberg.types.Types.NestedField.required;
 @Incubating
 public final class IcebergSchemaHistory extends AbstractSchemaHistory {
 
-  static final Schema DATABASE_HISTORY_TABLE_SCHEMA = new Schema(
-      required(1, "id", Types.StringType.get()),
-      optional(2, "history_data", Types.StringType.get()),
-      optional(3, "record_insert_ts", Types.TimestampType.withZone()
-      )
-  );
+  static final Schema DATABASE_HISTORY_TABLE_SCHEMA =
+      new Schema(
+          required(1, "id", Types.StringType.get()),
+          optional(2, "history_data", Types.StringType.get()),
+          optional(3, "record_insert_ts", Types.TimestampType.withZone()));
   private static final Logger LOG = LoggerFactory.getLogger(IcebergSchemaHistory.class);
   private final FunctionalReadWriteLock lock = FunctionalReadWriteLock.reentrant();
   private final DocumentWriter writer = DocumentWriter.defaultWriter();
@@ -87,7 +85,11 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
   OutputFileFactory fileFactory;
 
   @Override
-  public void configure(Configuration config, HistoryRecordComparator comparator, SchemaHistoryListener listener, boolean useCatalogBeforeSchema) {
+  public void configure(
+      Configuration config,
+      HistoryRecordComparator comparator,
+      SchemaHistoryListener listener,
+      boolean useCatalogBeforeSchema) {
     super.configure(config, comparator, listener, useCatalogBeforeSchema);
     this.storageConfig = new IcebergSchemaHistoryConfig(config, CONFIGURATION_FIELD_PREFIX_STRING);
     icebergCatalog = storageConfig.icebergCatalog();
@@ -95,7 +97,8 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
     tableId = storageConfig.tableIdentifier();
 
     if (running.get()) {
-      throw new SchemaHistoryException("Iceberg database history process already initialized table: " + tableFullName);
+      throw new SchemaHistoryException(
+          "Iceberg database history process already initialized table: " + tableFullName);
     }
   }
 
@@ -103,18 +106,19 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
   public void start() {
     super.start();
     LOG.info("Starting IcebergSchemaHistory storage table:" + tableFullName);
-    lock.write(() -> {
-      if (running.compareAndSet(false, true)) {
-        try {
-          if (!storageExists()) {
-            initializeStorage();
+    lock.write(
+        () -> {
+          if (running.compareAndSet(false, true)) {
+            try {
+              if (!storageExists()) {
+                initializeStorage();
+              }
+            } catch (Exception e) {
+              throw new SchemaHistoryException(
+                  "Unable to create history table: " + tableFullName + " : " + e.getMessage(), e);
+            }
           }
-        } catch (Exception e) {
-          throw new SchemaHistoryException("Unable to create history table: " + tableFullName + " : " + e.getMessage(),
-              e);
-        }
-      }
-    });
+        });
 
     historyTable = icebergCatalog.loadTable(tableId);
     format = IcebergUtil.getTableFileFormat(historyTable);
@@ -131,37 +135,45 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
     if (record == null) {
       return;
     }
-    lock.write(() -> {
-      if (!running.get()) {
-        throw new DebeziumException("The history has been stopped and will not accept more records");
-      }
-      try {
-        String recordDocString = writer.write(record.document());
-        LOG.trace("Saving history data {}", recordDocString);
-        OffsetDateTime currentTs = OffsetDateTime.now(ZoneOffset.UTC);
-        /// iceberg append
-        GenericRecord icebergRecord = GenericRecord.create(DATABASE_HISTORY_TABLE_SCHEMA);
-        Record row = icebergRecord.copy(
-            "id", UUID.randomUUID().toString(),
-            "history_data", recordDocString,
-            "record_insert_ts", currentTs
-        );
+    lock.write(
+        () -> {
+          if (!running.get()) {
+            throw new DebeziumException(
+                "The history has been stopped and will not accept more records");
+          }
+          try {
+            String recordDocString = writer.write(record.document());
+            LOG.trace("Saving history data {}", recordDocString);
+            OffsetDateTime currentTs = OffsetDateTime.now(ZoneOffset.UTC);
+            /// iceberg append
+            GenericRecord icebergRecord = GenericRecord.create(DATABASE_HISTORY_TABLE_SCHEMA);
+            Record row =
+                icebergRecord.copy(
+                    "id", UUID.randomUUID().toString(),
+                    "history_data", recordDocString,
+                    "record_insert_ts", currentTs);
 
-        try (BaseTaskWriter<Record> writer = new UnpartitionedWriter<>(
-            historyTable.spec(), format, appenderFactory, fileFactory, historyTable.io(), Long.MAX_VALUE)) {
-          writer.write(row);
-          writer.close();
-          WriteResult files = writer.complete();
+            try (BaseTaskWriter<Record> writer =
+                new UnpartitionedWriter<>(
+                    historyTable.spec(),
+                    format,
+                    appenderFactory,
+                    fileFactory,
+                    historyTable.io(),
+                    Long.MAX_VALUE)) {
+              writer.write(row);
+              writer.close();
+              WriteResult files = writer.complete();
 
-          Transaction t = historyTable.newTransaction();
-          Arrays.stream(files.dataFiles()).forEach(f -> t.newAppend().appendFile(f).commit());
-          t.commitTransaction();
-          LOG.trace("Successfully saved history data to Iceberg table");
-        }
-      } catch (IOException e) {
-        throw new SchemaHistoryException("Failed to store record: " + record, e);
-      }
-    });
+              Transaction t = historyTable.newTransaction();
+              Arrays.stream(files.dataFiles()).forEach(f -> t.newAppend().appendFile(f).commit());
+              t.commitTransaction();
+              LOG.trace("Successfully saved history data to Iceberg table");
+            }
+          } catch (IOException e) {
+            throw new SchemaHistoryException("Failed to store record: " + record, e);
+          }
+        });
   }
 
   @Override
@@ -172,29 +184,29 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
 
   @Override
   protected synchronized void recoverRecords(Consumer<HistoryRecord> records) {
-    lock.write(() -> {
-      if (exists()) {
-        try (CloseableIterable<Record> rs = IcebergGenerics.read(historyTable)
-            .build()) {
-          for (Record row : rs) {
-            String line = (String) row.getField("history_data");
-            if (line == null) {
-              break;
-            }
-            if (!line.isEmpty()) {
-              records.accept(new HistoryRecord(reader.read(line)));
+    lock.write(
+        () -> {
+          if (exists()) {
+            try (CloseableIterable<Record> rs = IcebergGenerics.read(historyTable).build()) {
+              for (Record row : rs) {
+                String line = (String) row.getField("history_data");
+                if (line == null) {
+                  break;
+                }
+                if (!line.isEmpty()) {
+                  records.accept(new HistoryRecord(reader.read(line)));
+                }
+              }
+            } catch (IOException e) {
+              throw new RuntimeException(e);
             }
           }
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-      }
-    });
+        });
   }
 
   @Override
   public boolean storageExists() {
-      return icebergCatalog.tableExists(tableId);
+    return icebergCatalog.tableExists(tableId);
   }
 
   @Override
@@ -205,8 +217,7 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
     }
 
     int numRows = 0;
-    try (CloseableIterable<Record> rs = IcebergGenerics.read(historyTable)
-        .build()) {
+    try (CloseableIterable<Record> rs = IcebergGenerics.read(historyTable).build()) {
       for (Record ignored : rs) {
         numRows++;
         break;
@@ -219,7 +230,8 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
 
   @Override
   public String toString() {
-    return "Iceberg database history storage: " + (tableFullName != null ? tableFullName : "(unstarted)");
+    return "Iceberg database history storage: "
+        + (tableFullName != null ? tableFullName : "(unstarted)");
   }
 
   @Override
@@ -227,7 +239,8 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
     if (!storageExists()) {
       try {
         LOG.info("Creating table {} to store database history", tableFullName);
-        historyTable = IcebergUtil.createIcebergTable(icebergCatalog, tableId, DATABASE_HISTORY_TABLE_SCHEMA);
+        historyTable =
+            IcebergUtil.createIcebergTable(icebergCatalog, tableId, DATABASE_HISTORY_TABLE_SCHEMA);
         LOG.warn("Created database history storage table {} to store history", tableFullName);
 
         if (!Strings.isNullOrEmpty(storageConfig.getMigrateHistoryFile().strip())) {
@@ -235,7 +248,8 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
           this.loadFileSchemaHistory(new File(storageConfig.getMigrateHistoryFile()));
         }
       } catch (Exception e) {
-        throw new SchemaHistoryException("Creation of database history topic failed, please create the topic manually", e);
+        throw new SchemaHistoryException(
+            "Creation of database history topic failed, please create the topic manually", e);
       }
     } else {
       LOG.debug("Storage is exists, skipping initialization");
@@ -243,28 +257,32 @@ public final class IcebergSchemaHistory extends AbstractSchemaHistory {
   }
 
   private void loadFileSchemaHistory(File file) {
-    LOG.warn(String.format("Migrating file database history from:'%s' to Iceberg database history storage: %s",
-        file.toPath(), tableFullName));
+    LOG.warn(
+        String.format(
+            "Migrating file database history from:'%s' to Iceberg database history storage: %s",
+            file.toPath(), tableFullName));
     AtomicInteger numRecords = new AtomicInteger();
-    lock.write(() -> {
-      try (BufferedReader historyReader = Files.newBufferedReader(file.toPath())) {
-        while (true) {
-          String line = historyReader.readLine();
-          if (line == null) {
-            break;
+    lock.write(
+        () -> {
+          try (BufferedReader historyReader = Files.newBufferedReader(file.toPath())) {
+            while (true) {
+              String line = historyReader.readLine();
+              if (line == null) {
+                break;
+              }
+              if (!line.isEmpty()) {
+                this.storeRecord(new HistoryRecord(reader.read(line)));
+                numRecords.getAndIncrement();
+              }
+            }
+          } catch (IOException e) {
+            logger.error(
+                "Failed to migrate history record from history file at {}", file.toPath(), e);
           }
-          if (!line.isEmpty()) {
-            this.storeRecord(new HistoryRecord(reader.read(line)));
-            numRecords.getAndIncrement();
-          }
-        }
-      } catch (IOException e) {
-        logger.error("Failed to migrate history record from history file at {}", file.toPath(), e);
-      }
-    });
-    LOG.warn("Migrated {} database history record. " +
-             "Migrating file database history to Iceberg database history storage successfully completed", numRecords.get());
+        });
+    LOG.warn(
+        "Migrated {} database history record. "
+            + "Migrating file database history to Iceberg database history storage successfully completed",
+        numRecords.get());
   }
-
-
 }
--- debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/history/IcebergSchemaHistoryTest.java
+++ debezium-server-iceberg-sink/src/test/java/io/debezium/server/iceberg/history/IcebergSchemaHistoryTest.java
@@ -8,6 +8,8 @@
 
 package io.debezium.server.iceberg.history;
 
+import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_CATALOG_TABLE_NAMESPACE;
+
 import io.debezium.server.iceberg.BaseSparkTest;
 import io.debezium.server.iceberg.testresources.CatalogNessie;
 import io.debezium.server.iceberg.testresources.S3Minio;
@@ -16,21 +18,19 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
-import java.sql.SQLException;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
-import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_CATALOG_TABLE_NAMESPACE;
-
 /**
- * Integration test that verifies basic reading from PostgreSQL database and writing to iceberg destination.
+ * Integration test that verifies basic reading from PostgreSQL database and writing to iceberg
+ * destination.
  *
  * @author Ismail Simsek
  */
@@ -43,51 +43,65 @@ import static io.debezium.server.iceberg.TestConfigSource.ICEBERG_CATALOG_TABLE_
 public class IcebergSchemaHistoryTest extends BaseSparkTest {
   @Test
   public void testSimpleUpload() throws SQLException, ClassNotFoundException {
-    String sqlCreate = "CREATE TABLE IF NOT EXISTS inventory.test_schema_history_ddl (" +
-        " c_id INTEGER ," +
-        " c_data TEXT," +
-        "  PRIMARY KEY (c_id)" +
-        " );";
+    String sqlCreate =
+        "CREATE TABLE IF NOT EXISTS inventory.test_schema_history_ddl ("
+            + " c_id INTEGER ,"
+            + " c_data TEXT,"
+            + "  PRIMARY KEY (c_id)"
+            + " );";
     SourceMysqlDB.runSQL(sqlCreate);
     String sqlInsert =
-        "INSERT INTO inventory.test_schema_history_ddl (c_id, c_data ) " +
-            "VALUES  (1,'data-1'),(2,'data-2'),(3,'data-3');";
+        "INSERT INTO inventory.test_schema_history_ddl (c_id, c_data ) "
+            + "VALUES  (1,'data-1'),(2,'data-2'),(3,'data-3');";
     SourceMysqlDB.runSQL(sqlInsert);
 
-    Awaitility.await().atMost(Duration.ofSeconds(160)).until(() -> {
-      try {
-        Dataset<Row> ds = getTableData("testc.inventory.test_schema_history_ddl");
-        return ds.count() >= 2;
-      } catch (Exception e) {
-//        e.printStackTrace();
-        return false;
-      }
-    });
+    Awaitility.await()
+        .atMost(Duration.ofSeconds(160))
+        .until(
+            () -> {
+              try {
+                Dataset<Row> ds = getTableData("testc.inventory.test_schema_history_ddl");
+                return ds.count() >= 2;
+              } catch (Exception e) {
+                //        e.printStackTrace();
+                return false;
+              }
+            });
 
     // test nested data(struct) consumed
-    Awaitility.await().atMost(Duration.ofSeconds(120)).until(() -> {
-      try {
-        Dataset<Row> ds = getTableData(ICEBERG_CATALOG_TABLE_NAMESPACE, "debezium_database_history_storage_table");
-        ds.show(10, false);
-        return ds.count() > 1
-            && ds.where("history_data ILIKE '%CREATE%TABLE%test_schema_history_ddl%'").count() == 1
-            ;
-      } catch (Exception e) {
-        return false;
-      }
-    });
+    Awaitility.await()
+        .atMost(Duration.ofSeconds(120))
+        .until(
+            () -> {
+              try {
+                Dataset<Row> ds =
+                    getTableData(
+                        ICEBERG_CATALOG_TABLE_NAMESPACE, "debezium_database_history_storage_table");
+                ds.show(10, false);
+                return ds.count() > 1
+                    && ds.where("history_data ILIKE '%CREATE%TABLE%test_schema_history_ddl%'")
+                            .count()
+                        == 1;
+              } catch (Exception e) {
+                return false;
+              }
+            });
   }
-
 
   public static class TestProfile implements QuarkusTestProfile {
     @Override
     public Map<String, String> getConfigOverrides() {
       Map<String, String> config = new HashMap<>();
       config.put("quarkus.profile", "mysql");
-      config.put("%mysql.debezium.source.connector.class", "io.debezium.connector.mysql.MySqlConnector");
-//      config.put("%mysql.debezium.source.table.whitelist", "inventory.*");
-      config.put("debezium.source.schema.history.internal", "io.debezium.server.iceberg.history.IcebergSchemaHistory");
-      config.put("debezium.source.schema.history.internal.iceberg.table-name", "debezium_database_history_storage_table");
+      config.put(
+          "%mysql.debezium.source.connector.class", "io.debezium.connector.mysql.MySqlConnector");
+      //      config.put("%mysql.debezium.source.table.whitelist", "inventory.*");
+      config.put(
+          "debezium.source.schema.history.internal",
+          "io.debezium.server.iceberg.history.IcebergSchemaHistory");
+      config.put(
+          "debezium.source.schema.history.internal.iceberg.table-name",
+          "debezium_database_history_storage_table");
       config.put("debezium.source.table.whitelist", "inventory.test_schema_history_ddl");
       return config;
     }
